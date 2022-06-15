034a9afb021a6de6e69e37a345b698356615ea34
noornee <kendrickbon11@gmail.com>
2022-06-15 21:25:12 +0100

    create task

--- task/cmd/add.go
+++ task/cmd/add.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/noornee/golang_practice_repo/task/db"
 	"github.com/spf13/cobra"
 )
 
@@ -10,6 +12,12 @@ var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Adds task to task list",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
+		task := strings.Join(args, " ")
+		_, err := db.CreateTask(task)
+		if err != nil {
+			fmt.Println("there was an error creating task", err)
+			return
+		}
+		fmt.Printf("successfully added \"%s\" to task list\n", task)
 	},
 }
--- task/main.go
+++ task/main.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 
 	"github.com/noornee/golang_practice_repo/task/cmd"
+	"github.com/noornee/golang_practice_repo/task/db"
 )
 
 func main() {
-	err := cmd.RootCmd.Execute()
+	errHandler(db.InitDB("task.db"))
+	errHandler(cmd.RootCmd.Execute())
+}
+
+func errHandler(err error) {
 	if err != nil {
 		fmt.Println(err)
 	}
