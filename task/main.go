cbba3301434b798e6647f95021c768550fe30d8c
noornee <kendrickbon11@gmail.com>
2022-06-15 10:52:41 +0100

    execute

--- task/main.go
+++ task/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/noornee/golang_practice_repo/task/cmd"
+)
+
+func main() {
+	err := cmd.RootCmd.Execute()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
